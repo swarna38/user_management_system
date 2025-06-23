692f24991ec435d470f80e56689d64412b3b726a
swarna38 <swarna.sutradhar.info@gamil.com>
2025-06-24 00:08:38 +0600

    user management connection setup

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php 
+
+$server = "localhost"; //servername
+$user = "root"; //username
+$password = ""; //password
+
+//db connection 
+$connection = new mysqli($server, $user, $password);
+echo "connection is success \n";
+?>
+
+
+
+
+
+
